0c368a5a876fb62e91f0cce0d53a7f50e1282726
efitz11 <efitz11@gmail.com>
2017-10-24 18:28:05 -0400

    better formatting for scores for mlb

--- discordtest.py
+++ discordtest.py
@@ -75,18 +75,20 @@ async def gif(*name : str):
     
 def get_game_str(gameid, lastplay=False):
     overview = mymlbgame.retoverview(gameid)
-    #print(overview)
     
     hometeam = overview['home_name_abbrev']
     awayteam = overview['away_name_abbrev']
     status   = overview['status']
-    if status == 'Preview':
+    
+    if status == 'Pre-Game' or status == 'Warmup' or status == 'Preview':
         firstpitch = overview['first_pitch_et']
         awins = overview['away_win']
         aloss = overview['away_loss']
         hwins = overview['home_win']
         hloss = overview['home_loss']
-        output = "**%s** (%s-%s) @ **%s** (%s-%s) - %s ET\n\t" % (awayteam, awins, aloss, hometeam, hwins,hloss, firstpitch)
+        hrecord = "(%s-%s)" % (hwins,hloss)
+        arecord = "(%s-%s)" % (awins,aloss)
+        output = "%s %s @ %s %s # %s ET - %s\n\t" % (awayteam.ljust(3), arecord, hometeam.ljust(3), hrecord, firstpitch,status)
         output = output + overview['away_probable_pitcher'] + " v " + overview['home_probable_pitcher']
         return output
         
@@ -97,28 +99,18 @@ def get_game_str(gameid, lastplay=False):
     top_inn  = "Top" if overview['top_inning'] == 'Y' else "Bot"
     outs = outs + " out" + ("" if outs == "1" else "s")
     
-    
-    output = "**%s %s** @ **%s %s**" % (awayteam, awayruns, hometeam, homeruns)
-    if status == 'Pre-Game':
-        firstpitch = overview['first_pitch_et']
-        awins = overview['away_win']
-        aloss = overview['away_loss']
-        hwins = overview['home_win']
-        hloss = overview['home_loss']
-        output = "**%s** (%s-%s) @ **%s** (%s-%s) - %s ET - %s\n\t" % (awayteam, awins, aloss, hometeam, hwins,hloss, firstpitch,status)
-        output = output + overview['away_probable_pitcher'] + " v " + overview['home_probable_pitcher']
-        return output
+    output = "%s %s @ %s %s" % (awayteam.ljust(3), awayruns.rjust(2), hometeam.ljust(3), homeruns.rjust(2))
     if status != 'Final':
         bases = ""
         if 'runner_on_base_status' in overview:
             bases = basesmap[overview['runner_on_base_status']]
         output = output + ":  %s %s - %s %s" % (top_inn, inning, outs, bases)
     else:
-        output = output + " (F)"
+        output = output + " (F) "
         wp = overview['winning_pitcher']
         lp = overview['losing_pitcher']
         sp = overview['save_pitcher']
-        output = output + "\tWP: " + wp + "\tLP: " + lp + (("\tSV: " + sp) if sp != "" else "")
+        output = output + "# WP: " + wp.ljust(10) + "\tLP: " + lp.ljust(10) + (("\tSV: " + sp.ljust(10)) if sp != "" else "")
     if lastplay and 'pbp_last' in overview:
         pitcher = overview['current_pitcher']
         batter = overview['current_batter']
@@ -132,10 +124,10 @@ async def mlb(*team :str):
     now = datetime.now() - timedelta(hours=3)
     if len(team) == 0:
         day = mlbgame.day(now.year, now.month, now.day)
-        output = "Today's scores:\n"
+        output = "Today's scores:\n```python\n"
         for game in day:
             output = output + get_game_str(game.game_id) +'\n'
-        await bot.say(output.strip())
+        await bot.say(output.strip() + "```")
         return
     
     teamname = team[0].title()
@@ -151,10 +143,10 @@ async def mlb(*team :str):
 async def mlbd(year:int, month:int, day:int, *team:str):
     if len(team) == 0:
         gameday = mlbgame.day(year, month, day)
-        output = "The day's scores:\n"
+        output = "The day's scores:\n```python\n"
         for game in gameday:
             output = output + get_game_str(game.game_id) +'\n'
-        await bot.say(output.strip())
+        await bot.say(output.strip() + "```")
         return
     else:
         team = team[0].title()
