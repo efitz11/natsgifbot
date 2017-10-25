68a411fe01befe23fae859433a6469010d474beb
efitz11 <efitz11@gmail.com>
2017-10-24 20:31:22 -0400

    working game updates with 15s delay for WS

--- discordtest.py
+++ discordtest.py
@@ -11,15 +11,6 @@ import mymlbgame, cfbgame, nflgame
 
 bot = commands.Bot(command_prefix='!')
 
-basesmap = {'0':'---',
-            '1':'1--',
-            '2':'-2-',
-            '3':'--3',
-            '4':'12-',
-            '5':'1-3',
-            '6':'-23',
-            '7':'123'}
-
 pattern69 = re.compile('(^|[\s\.])[6][9]([\s\.]|$)')
     
 # get tokens from file
@@ -72,52 +63,7 @@ async def gif(*name : str):
         await bot.say(matches[num].strip())
             
     return
-    
-def get_game_str(gameid, lastplay=False):
-    overview = mymlbgame.retoverview(gameid)
-    
-    hometeam = overview['home_name_abbrev']
-    awayteam = overview['away_name_abbrev']
-    status   = overview['status']
-    
-    if status == 'Pre-Game' or status == 'Warmup' or status == 'Preview':
-        firstpitch = overview['first_pitch_et']
-        awins = overview['away_win']
-        aloss = overview['away_loss']
-        hwins = overview['home_win']
-        hloss = overview['home_loss']
-        hrecord = "(%s-%s)" % (hwins,hloss)
-        arecord = "(%s-%s)" % (awins,aloss)
-        output = "%s %s @ %s %s # %s ET - %s\n\t" % (awayteam.ljust(3), arecord, hometeam.ljust(3), hrecord, firstpitch,status)
-        output = output + overview['away_probable_pitcher'] + " v " + overview['home_probable_pitcher']
-        return output
-        
-    homeruns = overview['home_team_runs']
-    awayruns = overview['away_team_runs']
-    outs     = overview['outs']
-    inning   = overview['inning']
-    top_inn  = "Top" if overview['top_inning'] == 'Y' else "Bot"
-    outs = outs + " out" + ("" if outs == "1" else "s")
-    
-    output = "%s %s @ %s %s" % (awayteam.ljust(3), awayruns.rjust(2), hometeam.ljust(3), homeruns.rjust(2))
-    if status != 'Final':
-        bases = ""
-        if 'runner_on_base_status' in overview:
-            bases = basesmap[overview['runner_on_base_status']]
-        output = output + ":  %s %s - %s %s" % (top_inn, inning, outs, bases)
-    else:
-        output = output + " (F) "
-        wp = overview['winning_pitcher']
-        lp = overview['losing_pitcher']
-        sp = overview['save_pitcher']
-        output = output + "# WP: " + wp.ljust(10) + "\tLP: " + lp.ljust(10) + (("\tSV: " + sp.ljust(10)) if sp != "" else "")
-    if lastplay and 'pbp_last' in overview:
-        pitcher = overview['current_pitcher']
-        batter = overview['current_batter']
-        lplay    = overview['pbp_last']
-        output = output + "\n\tPitching: **%s** \tBatting: **%s**" % (pitcher, batter)
-        output = output + "\n\tLast play: *" + lplay.strip() + "*"
-    return output
+
     
 @bot.command()
 async def mlb(*team :str):
@@ -126,7 +72,7 @@ async def mlb(*team :str):
         day = mlbgame.day(now.year, now.month, now.day)
         output = "Today's scores:\n```python\n"
         for game in day:
-            output = output + get_game_str(game.game_id) +'\n'
+            output = output + mymlbgame.get_game_str(game.game_id) +'\n'
         await bot.say(output.strip() + "```")
         return
     
@@ -136,8 +82,8 @@ async def mlb(*team :str):
     if len(day) > 0 :
         game = day[0]
         id = game.game_id
-        output = get_game_str(id,lastplay=True)
-        await bot.say(output)
+        output = mymlbgame.get_game_str(id,lastplay=True)
+        await bot.say("```python\n" + output + "```")
 
 @bot.command()
 async def mlbd(year:int, month:int, day:int, *team:str):
@@ -145,7 +91,7 @@ async def mlbd(year:int, month:int, day:int, *team:str):
         gameday = mlbgame.day(year, month, day)
         output = "The day's scores:\n```python\n"
         for game in gameday:
-            output = output + get_game_str(game.game_id) +'\n'
+            output = output + mymlbgame.get_game_str(game.game_id) +'\n'
         await bot.say(output.strip() + "```")
         return
     else:
@@ -272,16 +218,29 @@ async def on_message(message):
         if pattern69.search(message.content):
             await bot.send_message(message.channel, 'Nice.')
 
-    
+updater = mymlbgame.Updater()
+
 async def my_bg_task():
     await bot.wait_until_ready()
     channel = discord.Object(id = main_chid)
     while not bot.is_closed:
-        await asyncio.sleep(100)
+        #print("update")
+        teamname = "Astros"
+        now = datetime.now() - timedelta(hours=3)
+        day = mlbgame.day(now.year, now.month, now.day, home=teamname, away=teamname)
+        
+        if len(day) > 0 :
+            game = day[0]
+            id = game.game_id
+            output = updater.update(id)
+            if output != "" and output != None:
+                output = "```python\n" + output + "```"
+                await bot.send_message(channel,output)
+        await asyncio.sleep(15)
 
 reddit = praw.Reddit(client_id=reddit_clientid,
                      client_secret=reddit_token,
                      user_agent='windows:natsgifbot (by /u/efitz11)')
 print(reddit.read_only)
-#bot.loop.create_task(my_bg_task())
+bot.loop.create_task(my_bg_task())
 bot.run(discord_token)
--- mymlbgame.py
+++ mymlbgame.py
@@ -8,6 +8,15 @@ BASE_URL = ('http://gd2.mlb.com/components/game/mlb/'
             'year_{0}/month_{1:02d}/day_{2:02d}/')
 GAME_URL = BASE_URL + '/gid_{3}/{4}'
 
+basesmap = {'0':'---',
+            '1':'1--',
+            '2':'-2-',
+            '3':'--3',
+            '4':'12-',
+            '5':'1-3',
+            '6':'-23',
+            '7':'123'}
+
 def get_date_from_game_id(game_id):
     year, month, day, _discard = game_id.split('_', 3)
     return int(year), int(month), int(day)
@@ -45,3 +54,73 @@ def retoverview(game_id):
         output['losing_pitcher'] = parsed.find('losing_pitcher').get("last_name")
         output['save_pitcher'] = parsed.find('save_pitcher').get("last_name")
     return output
+    
+    
+def get_game_str(gameid, lastplay=False):
+    overview = retoverview(gameid)
+    return get_game_status(overview, lastplay)
+
+def get_game_status(overview, lastplay=False):
+    hometeam = overview['home_name_abbrev']
+    awayteam = overview['away_name_abbrev']
+    status   = overview['status']
+    
+    if status == 'Pre-Game' or status == 'Warmup' or status == 'Preview':
+        firstpitch = overview['first_pitch_et']
+        awins = overview['away_win']
+        aloss = overview['away_loss']
+        hwins = overview['home_win']
+        hloss = overview['home_loss']
+        hrecord = "(%s-%s)" % (hwins,hloss)
+        arecord = "(%s-%s)" % (awins,aloss)
+        output = "%s %s @ %s %s # %s ET - %s\n\t" % (awayteam.ljust(3), arecord, hometeam.ljust(3), hrecord, firstpitch,status)
+        output = output + overview['away_probable_pitcher'] + " v " + overview['home_probable_pitcher']
+        return output
+        
+    homeruns = overview['home_team_runs']
+    awayruns = overview['away_team_runs']
+    outs     = overview['outs']
+    inning   = overview['inning']
+    top_inn  = "Top" if overview['top_inning'] == 'Y' else "Bot"
+    outs = outs + " out" + ("" if outs == "1" else "s")
+    
+    output = "%s %s @ %s %s" % (awayteam, awayruns, hometeam, homeruns)
+    if status != 'Final':
+        bases = ""
+        if 'runner_on_base_status' in overview:
+            bases = basesmap[overview['runner_on_base_status']]
+        output = output + ":  %s %s - %s %s" % (top_inn, inning, outs, bases)
+    else:
+        output = output + " (F) "
+        wp = overview['winning_pitcher']
+        lp = overview['losing_pitcher']
+        sp = overview['save_pitcher']
+        output = output + "# WP: " + wp.ljust(10) + "\tLP: " + lp.ljust(10) + (("\tSV: " + sp.ljust(10)) if sp != "" else "")
+    if lastplay and 'pbp_last' in overview:
+        pitcher = overview['current_pitcher']
+        batter = overview['current_batter']
+        lplay    = overview['pbp_last']
+        output = output + "\n\tPitching: %s \tBatting: %s" % (pitcher, batter)
+        output = output + "\n\tLast play: " + lplay.strip()
+    return output
+    
+class Updater:
+    
+    def __init__(self):
+        self.lastpbp = ""
+        self.updatenext = False
+        self.nextupdate = "" 
+    def update(self,id):
+        output = ""
+        overview = retoverview(id)
+        #print("updating")
+        if self.updatenext: #delay block
+            self.updatenext = False
+            output = self.nextupdate
+        if 'pbp_last' in overview:
+            if overview['pbp_last'] != self.lastpbp:
+                print (overview['pbp_last'])
+                self.lastpbp = overview['pbp_last']
+                self.updatenext = True
+                self.nextupdate = get_game_status(overview,lastplay=True)
+        return output
