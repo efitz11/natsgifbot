c3305db97bb6fe17e3a4c8534f89abc60880e15b
efitz11 <efitz11@gmail.com>
2017-11-20 13:21:18 -0500

    Formatting fixes for stocks

--- stocks.py
+++ stocks.py
@@ -7,6 +7,15 @@ def get_quote(symbol):
     req.headers["User-Agent"] = "windows 10 bot"
     # Load data
     quote = json.loads(urlopen(req).read().decode("utf-8"))
-    output = "```python\n Last price: %s (%s,%s" % (quote['latestPrice'],quote['change'],quote['changePercent'])+"%)"
+    change = float(quote['change'])
+    ch = "%0.2f" %(change)
+    chper = "%0.2f" %(quote['changePercent'])
+    if change > 0:
+        ch = "+" + ch
+        chper = "+" + chper
+    elif change < 0:
+        ch = "-" + ch
+        chper = "-" + chper
+    output = "%s quote:```python\n Last price: %s (%s, %s" % (symbol.upper(),quote['latestPrice'],ch,chper)+"%)"
     output = output + "```"
     return output
