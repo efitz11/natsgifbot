09a106752939d64ee21326eaeb023cbb0be24f77
efitz11 <efitz11@gmail.com>
2017-12-11 17:48:01 -0500

    initial frink meme support

--- frinkiac.py
+++ frinkiac.py
@@ -7,17 +7,28 @@ def get_response(url):
     req.headers["User-Agent"] = "windows 10 bot"
     return urlopen(req).read().decode("utf-8")
     
-def get_meme(query):
+def get_ep_ts(query):
     url = "https://frinkiac.com/api/search?q="+urllib.parse.quote_plus(query)
     response = json.loads(get_response(url))
     ep = response[0]["Episode"]
     ts = response[0]["Timestamp"]
+    return (ep,ts)
+    
+def get_lines(ep,ts):
     url = "https://frinkiac.com/api/caption?e=%s&t=%s" % (ep,ts)
     print(url)
     response = json.loads(get_response(url))
-    #print(response)
-    subtitles = response["Subtitles"]
-    subs = ""
+    return response["Subtitles"]
+    
+def get_context_frames(ep,ts):
+    url = "https://frinkiac.com/api/frames/%s/%s/4000/4000" % (ep,ts)
+    response = json.loads(get_response(url))
+    return response
+    
+def get_meme(query):
+    ep,ts = get_ep_ts(query)
+    subs = get_lines(ep,ts)
+    
     for s in subtitles:
         line = s["Content"]
         words = line.split(' ')
@@ -33,3 +44,12 @@ def get_meme(query):
     subs = subs.strip()
     url = "https://frinkiac.com/meme/%s/%s.jpg?lines=%s" % (ep,ts,urllib.parse.quote_plus(subs))
     return(url)
+
+def get_gif(query):
+    ep,ts = get_ep_ts(query)
+    subs = get_lines(ep,ts)
+    context = get_context_frames(ep,ts)
+    url = "https://frinkiac.com/gif/%s/%s/%s.gif?lines=%s" %(ep,context[0]['Timestamp'],context[-1]['Timestamp'],urllib.parse.quote_plus(subs))
+    return url
+    
+    
