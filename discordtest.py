90bc291324d1c618cdd8b56e1dfb882d14b565b9
efitz11 <efitz11@gmail.com>
2018-01-04 10:46:28 -0500

    cheer reaction, snow showers emoji

--- discordtest.py
+++ discordtest.py
@@ -16,6 +16,7 @@ import frinkiac, cryptocurrency
 bot = commands.Bot(command_prefix='!')
 
 pattern69 = re.compile('(^|[\s\.])[6][\.]*[9]([\s\.]|x|%|$|th)')
+patterncheer = re.compile('cheer', re.IGNORECASE)
     
 # get tokens from file
 f = open('tokens.txt','r')
@@ -28,6 +29,32 @@ f = open('channelids.txt')
 main_chid = f.readline().strip()
 f.close()
 
+emoji_letter_map = {'a':u"\U0001F1E6",
+					'b':u"\U0001F1E7",
+					'c':u"\U0001F1E8",
+					'd':u"\U0001F1E9",
+					'e':u"\U0001F1EA",
+					'f':u"\U0001F1EB",
+					'g':u"\U0001F1EC",
+					'h':u"\U0001F1ED",
+					'i':u"\U0001F1EE",
+					'j':u"\U0001F1EF",
+					'k':u"\U0001F1F0",
+					'l':u"\U0001F1F1",
+					'm':u"\U0001F1F2",
+					'n':u"\U0001F1F3",
+					'o':u"\U0001F1F4",
+					'p':u"\U0001F1F5",
+					'q':u"\U0001F1F6",
+					'r':u"\U0001F1F7",
+					's':u"\U0001F1F8",
+					't':u"\U0001F1F9",
+					'u':u"\U0001F1FA",
+                    'v':u"\U0001F1FB",
+					'w':u"\U0001F1FC",
+					'x':u"\U0001F1FD",
+					'y':u"\U0001F1FE",
+					'z':u"\U0001F1FF",}
 
 @bot.event
 async def on_ready():
@@ -442,11 +469,15 @@ async def on_message(message):
         await bot.delete_message(message)
     else:
         if pattern69.search(message.content):
-            await bot.add_reaction(message, u"\U0001F1F3")
-            await bot.add_reaction(message, u"\U0001F1EE")
-            await bot.add_reaction(message, u"\U0001F1E8")
-            await bot.add_reaction(message, u"\U0001F1EA")
-            #await bot.send_message(message.channel, 'Nice.')
+            await bot.add_reaction(message, emoji_letter_map['n'])
+            await bot.add_reaction(message, emoji_letter_map['i'])
+            await bot.add_reaction(message, emoji_letter_map['c'])
+            await bot.add_reaction(message, emoji_letter_map['e'])
+        if patterncheer.search(message.content):
+            await bot.add_reaction(message, emoji_letter_map['n'])
+            await bot.add_reaction(message, emoji_letter_map['a'])
+            await bot.add_reaction(message, emoji_letter_map['t'])
+            await bot.add_reaction(message, emoji_letter_map['s'])
         mockobj.update(str(message.content),message.channel)
 
 updater = mymlbgame.Updater()
