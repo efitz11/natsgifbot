7ad60a7d44fb480b01ae280ea64d10e705053325
efitz11 <efitz11@gmail.com>
2018-01-17 19:23:10 -0500

    separated reddit commands

--- discordtest.py
+++ discordtest.py
@@ -15,7 +15,7 @@ import frinkiac, cryptocurrency, wikipedia
 
 bot = commands.Bot(command_prefix='!')
 
-extensions = ["baseball","sports"]
+extensions = ["baseball","sports","reddit"]
 
 pattern69 = re.compile('(^|[\s\.]|\$)[6][\.]*[9]([\s\.]|x|%|$|th)')
 patterncheer = re.compile('cheer', re.IGNORECASE)
@@ -132,116 +132,6 @@ async def gifall(*name:str):
         await bot.say(output.strip())
         
     return
-
-def sub(subreddit, selfpost=False):
-    list = []
-    try:
-        for submission in reddit.subreddit(subreddit).hot(limit=25):
-    #        if submission.is_self == selfpost and not submission.stickied and not submission.over_18:
-            if submission.is_self == selfpost and not submission.stickied:
-                if submission.is_self:
-                    list.append(submission.title)
-                else:
-                    url = submission.url
-                    s = ""
-                    if submission.over_18:
-                        s = "**post is NSFW; embed hidden**\n"
-                        url = "<" + url + ">"
-                    s = s + submission.title + "\n" + url + "  \t<" + submission.shortlink+">"
-                    list.append(s)
-        num = random.randint(0,len(list)-1)
-        return (list[num])
-    except prawcore.exceptions.Redirect:
-        return ("Error: subreddit not found")
-    
-@bot.command()
-async def r(text:str):
-    """<subreddit> get a random link post from a subreddit"""
-    await bot.say(sub(text))
-    
-def getsubmissiontext(submission):
-    url = submission.url
-    s = ""
-    if submission.over_18:
-        s = "**post is NSFW; embed hidden**\n"
-        url = "<" + url + ">"
-    score = getsubmissionscore(submission)
-    if submission.is_self:
-        output = submission.title + "\n" + submission.shortlink
-    else:
-        output = s+"["+ score + "]\t " + submission.title + "\n" + url + "  \t<" + submission.shortlink+">"
-    return output
-    
-def getsubmissionscore(submission):
-    score = submission.score
-    if score > 999:
-        score = str(int(score/100)/10.0) + "k"
-    else:
-        score = str(score)
-    return score
-    
-@bot.command()
-async def rh(text:str, num:int=-1):
-    """<num> <subreddit> get the #num post from subreddit/hot"""
-    #subreddit = ''.join(text).lower()
-    subreddit = text
-    count = 0
-    if num > 0:
-        for submission in reddit.subreddit(subreddit).hot(limit=(num+2)):
-            if submission.stickied:
-                continue
-            count += 1
-            if count == num:
-                output = getsubmissiontext(submission)
-                await bot.say(output)
-    else:
-        output = "10 hottest posts from r/%s\n" % text
-        for submission in reddit.subreddit(subreddit).hot(limit=10):
-            score = getsubmissionscore(submission)
-            output = output + "["+ score + "]\t " + submission.title + "\n\t\t<" + submission.shortlink + ">\n"
-        await bot.say(output)
-
-@bot.command()
-async def rn(text:str, num:int=-1):
-    """<num> <subreddit> get the #num post from subreddit/new"""
-    subreddit = text
-    count = 0
-    if num > 0:
-        for submission in reddit.subreddit(subreddit).new(limit=(num+2)):
-            count += 1
-            if count == num:
-                output = getsubmissiontext(submission)
-                await bot.say(output)
-    else:
-        output = "10 newest posts from r/%s\n" % text
-        for submission in reddit.subreddit(subreddit).new(limit=10):
-            score = getsubmissionscore(submission)
-            output = output + "["+ score + "]\t " + submission.title + "\n\t\t<" + submission.shortlink + ">\n"
-        await bot.say(output)
-            
-@bot.command()
-async def rs(subreddit:str, *query:str):
-    """<subreddit> get the first post (by hot) matching the query"""
-    patterns = []
-    for s in query:
-        patterns.append(re.compile(s,re.IGNORECASE))
-    if subreddit.endswith("/new"):
-        list = reddit.subreddit(subreddit[:subreddit.find("/")]).new(limit=100)
-    else:
-        i = subreddit.find("/")
-        if i != -1:
-            subreddit = subreddit[:i]
-        list = reddit.subreddit(subreddit).hot(limit=100)
-    for submission in list:
-        matched = True
-        for pat in patterns:
-            if not re.search(pat,submission.title):
-                matched = False
-                break
-            if matched:
-                output = getsubmissiontext(submission)
-                await bot.say(output)
-                return
                 
 def mockify_text(text):
     last = False
@@ -504,10 +394,7 @@ async def update_mlbtr():
 mockobj = mocker()
 mlbtr = xmlreader.XmlReader()
 
-reddit = praw.Reddit(client_id=reddit_clientid,
-                     client_secret=reddit_token,
-                     user_agent='windows:natsgifbot (by /u/efitz11)')
-print(reddit.read_only)
+#print(reddit.read_only)
 #bot.loop.create_task(my_bg_task())
 bot.loop.create_task(update_mlbtr())
 for ext in extensions:
---
+++ reddit.py
@@ -0,0 +1,135 @@
+import discord
+from discord.ext import commands
+import praw, prawcore.exceptions
+
+from urllib.request import urlopen, Request
+import urllib.parse
+from datetime import datetime, timedelta, time
+import random
+import re
+
+class Reddit():
+    def __init__(self,bot):
+        self.bot = bot
+        # get tokens from file
+        f = open('tokens.txt','r')
+        reddit_clientid = f.readline().strip()
+        reddit_token = f.readline().strip()
+        discord_token = f.readline().strip()
+        f.close()
+        self.reddit = praw.Reddit(client_id=reddit_clientid,
+                     client_secret=reddit_token,
+                     user_agent='windows:natsgifbot (by /u/efitz11)')
+        
+    def sub(self, subreddit, selfpost=False):
+        list = []
+        try:
+            for submission in self.reddit.subreddit(subreddit).hot(limit=25):
+        #        if submission.is_self == selfpost and not submission.stickied and not submission.over_18:
+                if submission.is_self == selfpost and not submission.stickied:
+                    if submission.is_self:
+                        list.append(submission.title)
+                    else:
+                        url = submission.url
+                        s = ""
+                        if submission.over_18:
+                            s = "**post is NSFW; embed hidden**\n"
+                            url = "<" + url + ">"
+                        s = s + submission.title + "\n" + url + "  \t<" + submission.shortlink+">"
+                        list.append(s)
+            num = random.randint(0,len(list)-1)
+            return (list[num])
+        except prawcore.exceptions.Redirect:
+            return ("Error: subreddit not found")
+        
+    @commands.command()
+    async def r(self,text:str):
+        """<subreddit> get a random link post from a subreddit"""
+        await self.bot.say(self.sub(text))
+        
+    def getsubmissiontext(self, submission):
+        url = submission.url
+        s = ""
+        if submission.over_18:
+            s = "**post is NSFW; embed hidden**\n"
+            url = "<" + url + ">"
+        score = self.getsubmissionscore(submission)
+        if submission.is_self:
+            output = submission.title + "\n" + submission.shortlink
+        else:
+            output = s+"["+ score + "]\t " + submission.title + "\n" + url + "  \t<" + submission.shortlink+">"
+        return output
+        
+    def getsubmissionscore(self, submission):
+        score = submission.score
+        if score > 999:
+            score = str(int(score/100)/10.0) + "k"
+        else:
+            score = str(score)
+        return score
+        
+    @commands.command()
+    async def rh(self, text:str, num:int=-1):
+        """<num> <subreddit> get the #num post from subreddit/hot"""
+        #subreddit = ''.join(text).lower()
+        subreddit = text
+        count = 0
+        if num > 0:
+            for submission in self.reddit.subreddit(subreddit).hot(limit=(num+2)):
+                if submission.stickied:
+                    continue
+                count += 1
+                if count == num:
+                    output = self.getsubmissiontext(submission)
+                    await self.bot.say(output)
+        else:
+            output = "10 hottest posts from r/%s\n" % text
+            for submission in self.reddit.subreddit(subreddit).hot(limit=10):
+                score = self.getsubmissionscore(submission)
+                output = output + "["+ score + "]\t " + submission.title + "\n\t\t<" + submission.shortlink + ">\n"
+            await self.bot.say(output)
+
+    @commands.command()
+    async def rn(self, text:str, num:int=-1):
+        """<num> <subreddit> get the #num post from subreddit/new"""
+        subreddit = text
+        count = 0
+        if num > 0:
+            for submission in self.reddit.subreddit(subreddit).new(limit=(num+2)):
+                count += 1
+                if count == num:
+                    output = self.getsubmissiontext(submission)
+                    await self.bot.say(output)
+        else:
+            output = "10 newest posts from r/%s\n" % text
+            for submission in self.reddit.subreddit(subreddit).new(limit=10):
+                score = self.getsubmissionscore(submission)
+                output = output + "["+ score + "]\t " + submission.title + "\n\t\t<" + submission.shortlink + ">\n"
+            await self.bot.say(output)
+                
+    @commands.command()
+    async def rs(self, subreddit:str, *query:str):
+        """<subreddit> get the first post (by hot) matching the query"""
+        patterns = []
+        for s in query:
+            patterns.append(re.compile(s,re.IGNORECASE))
+        if subreddit.endswith("/new"):
+            list = self.reddit.subreddit(subreddit[:subreddit.find("/")]).new(limit=100)
+        else:
+            i = subreddit.find("/")
+            if i != -1:
+                subreddit = subreddit[:i]
+            list = self.reddit.subreddit(subreddit).hot(limit=100)
+        for submission in list:
+            matched = True
+            for pat in patterns:
+                if not re.search(pat,submission.title):
+                    matched = False
+                    break
+                if matched:
+                    output = self.getsubmissiontext(submission)
+                    await self.bot.say(output)
+                    return
+        
+def setup(bot):
+    bot.add_cog(Reddit(bot))
