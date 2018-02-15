c2661795a33d7a9b069d64117f6ccab9a1260cf6
efitz11 <efitz11@gmail.com>
2018-02-14 21:50:57 -0500

    working reddit bot

--- redditbot.py
+++ redditbot.py
@@ -1,22 +1,36 @@
 import praw, prawcore.exceptions
+import pprint
+import gifs
 
 class RedditBot():
     def __init__(self):
+        self.enabled_subs = ["computerdudetest"]
+    
         # get tokens from file
         f = open('reddittokens.txt','r')
         reddit_clientid = f.readline().strip()
         reddit_token = f.readline().strip()
         password = f.readline().strip()
         f.close()
-        print(reddit_clientid)
-        print(reddit_token)
         self.reddit = praw.Reddit(client_id=reddit_clientid,
                      client_secret=reddit_token,
                      user_agent='ubuntu:computer-dude (by /u/efitz11)',
                      username="computer-dude",password=password)
-        #print(self.reddit.user.me())
                      
     def check_mentions(self):
         print("checking messages")
-        for mention in self.reddit.inbox.mentions(limit=100):
-            print(mention.author, mention.body)
+        mention_str = "/u/computer-dude"
+        for comment in self.reddit.inbox.unread(limit=100):
+            if comment.subreddit.display_name.lower() in self.enabled_subs:
+                text = comment.body.lower()
+                if mention_str in text:
+                    text = text.replace(mention_str,'').strip()
+                    if text.startswith("gif"):
+                        text = text.replace("gif",'').strip()
+                        comment.reply(gifs.gif(text).replace(",","\n\n"))
+            comment.mark_read()
+            
+
+if __name__ == "__main__":
+    r = RedditBot()
+    r.check_mentions()
