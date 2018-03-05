553296a9e15baafc8feb3c481e5462bfdb52dd31
efitz11 <efitz11@gmail.com>
2018-03-04 23:36:28 -0500

    fungo got kicked, fun's over

--- discordtest.py
+++ discordtest.py
@@ -337,11 +337,7 @@ async def poll(question, *answers):
 @bot.command(pass_context=True)
 async def terminate(ctx):
     """no"""
-    if ctx.message.author.display_name == "Fungo":
-        await bot.kick(ctx.message.author)
-        await bot.say("User %s has been kicked for his/her actions." % ctx.message.author.mention)
-    else:
-        await bot.say("%s is not in the terminators file. This incident will be reported." % ctx.message.author.mention)
+    await bot.say("%s is not in the terminators file. This incident will be reported." % ctx.message.author.mention)
 
 @bot.command(pass_context=True)
 async def slap(ctx, *text:str):
