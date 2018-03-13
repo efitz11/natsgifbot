2ddf895b3f4e2bec8a152f44f7ef6d6672b002a3
efitz11 <efitz11@gmail.com>
2018-03-13 19:41:50 -0400

    instagram changed their json

--- instagram.py
+++ instagram.py
@@ -11,4 +11,4 @@ def get_latest_post(username):
     s = s[:s.find("};")+1]
     s = json.loads(s)
     #print("%s" % s['entry_data']['ProfilePage'][0]['user']['media']['nodes'][0]['code'])
-    return "https://www.instagram.com/p/" + s['entry_data']['ProfilePage'][0]['user']['media']['nodes'][0]['code']
+    return "https://www.instagram.com/p/" + s['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode']
