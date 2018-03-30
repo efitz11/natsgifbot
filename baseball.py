3e8c5494c25a71a75558deb3c16e2b7fa28351c5
efitz11 <efitz11@gmail.com>
2018-03-30 10:27:27 -0400

    nats=nationals override

--- baseball.py
+++ baseball.py
@@ -51,6 +51,8 @@ class Baseball():
             return
         
         teamname = team[0].title()
+        if teamname == "nats":
+            teamname = "nationals"
         day = mlbgame.day(now.year, now.month, now.day, home=teamname, away=teamname)
         
         if len(day) > 0 :
