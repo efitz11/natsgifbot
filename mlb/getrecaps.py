3e6158a6caa7af32bd211924df4eaf55248102cf
efitz11 <efitz11@gmail.com>
2018-04-03 15:19:41 -0400

    added finding of top 5 plays video

--- mlb/getrecaps.py
+++ mlb/getrecaps.py
@@ -24,6 +24,25 @@ def get_recaps():
                 recaps.append((title,link,duration))
     return recaps
 
+def get_direct_video_url(indirecturl):
+    url = indirecturl
+    one=url[-3]
+    two=url[-2]
+    thr=url[-1]
+    cid = url[url.index('c-')+2:]
+    mlburl = "http://www.mlb.com/gen/multimedia/detail/%s/%s/%s/%s.xml"%(one,two,thr,cid)
+    try:
+        req = Request(mlburl, headers={'User-Agent' : "ubuntu"})
+        tree = ElementTree.fromstring(urlopen(req).read().decode("utf-8"))
+        media = tree.findall('url')
+        for tag in media:
+            if "2500K.mp4" in tag.text:
+                url = tag.text
+                return url
+                break
+    except Exception as e:
+        print("error parsing/receiving XML from url " + mlburl)
+        return
 
 def find_fastcast():
     url = "https://search-api.mlb.com/svc/search/v2/mlb_global_sitesearch_en/sitesearch?hl=true&facet=type&expand=partner.media&q=fastcast&page=1"
@@ -34,27 +53,31 @@ def find_fastcast():
     if "MLB.com FastCast" in title:
         blurb = result['blurb']
         url = result['url']
-        one=url[-3]
-        two=url[-2]
-        thr=url[-1]
-        cid = url[url.index('c-')+2:]
-        mlburl = "http://www.mlb.com/gen/multimedia/detail/%s/%s/%s/%s.xml"%(one,two,thr,cid)
-        try:
-            req = Request(mlburl, headers={'User-Agent' : "ubuntu"})
-            tree = ElementTree.fromstring(urlopen(req).read().decode("utf-8"))
-            media = tree.findall('url')
-            for tag in media:
-                if "2500K.mp4" in tag.text:
-                    url = tag.text
-                    break
-        except Exception as e:
-            print("error parsing/receiving XML from url " + mlburl)
-            return
+        dir = get_direct_video_url(url)
+        if dir is not None:
+            url = dir
+        duration = result['duration'][3:]
+        print("[%s](%s) - %s\n" % (blurb,url,duration))
+        return (blurb,url,duration)
 
-        duration = result['duration']
+def find_top_plays():
+    url = "https://search-api.mlb.com/svc/search/v2/mlb_global_sitesearch_en/sitesearch?hl=true&facet=type&expand=partner.media&q=top%2B5%2Bplays&page=1"
+    req = Request(url, headers={'User-Agent' : "ubuntu"})
+    s = json.loads(urlopen(req).read().decode("utf-8"))
+    result = s['docs'][0]
+    blurb = result['blurb']
+    now = datetime.now() - timedelta(days=1)
+    date = "%d/%d/%s:" % (now.month, now.day, str(now.year)[2:])
+    if "Top 5 Plays" in blurb and blurb.startswith(date):
+        url = result['url']
+        dir = get_direct_video_url(url)
+        if dir is not None:
+            url = dir
+        duration = result['duration'][3:]
         print("[%s](%s) - %s\n" % (blurb,url,duration))
         return (blurb,url,duration)
 
 if __name__ == "__main__":
     find_fastcast()
+    find_top_plays()
     get_recaps()
