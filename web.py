641e9319940197908ef68db1ff53486cf6f973f0
efitz11 <efitz11@gmail.com>
2018-04-03 13:59:16 -0400

    move crypto into web.py

--- web.py
+++ web.py
@@ -42,5 +42,22 @@ def get_latest_ig_post(username):
     #print("%s" % s['entry_data']['ProfilePage'][0]['user']['media']['nodes'][0]['code'])
     return "https://www.instagram.com/p/" + s['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode']
 
+def get_cryptocurrency_data(text):
+    if len(text) == 0:
+        text = "?limit=10"
+    req = Request("https://api.coinmarketcap.com/v1/ticker/" +text)
+    req.headers["User-Agent"] = "windows 10 bot"
+    data = json.loads(urlopen(req).read().decode("utf-8"))
+    if "error" in data:
+        return
+    output = "```python\n"
+    for coin in data:
+        name = coin["name"]
+        price = coin["price_usd"]
+        change = coin["percent_change_24h"]
+        change7 = coin["percent_change_7d"]
+        output = output + name.ljust(12) + " $" + price.ljust(10) + change.rjust(6) + "% last 24h\t" + change7.rjust(6) + "% last 7d\n"
+    return output+"```"
+
 if __name__ == "__main__":
     print(search_untappd("heineken"))
