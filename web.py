ce4342bbb0ad960171e94cac51f99b93d2d4d2ec
efitz11 <efitz11@gmail.com>
2018-04-03 13:57:08 -0400

    moved instagram into web.py

--- web.py
+++ web.py
@@ -29,6 +29,18 @@ def search_untappd(beer_name):
     beer_ibu =  soup1.find('p',class_='ibu').get_text().strip()
     rating =    soup1.find('p',class_='rating').get_text().strip()
     return  "**%s** - %s \t<https://untappd.com%s>\n%s\t %s\t %s\t Rating: %s" % (beer_name,brewery,href,beer_type,beer_abv,beer_ibu,rating)
-    
+
+def get_latest_ig_post(username):
+    url = "https://www.instagram.com/" + username
+    req = Request(url, headers={'User-Agent' : "ubuntu"})
+    #req.headers["User-Agent"] = "windows 10 bot"
+    s = urlopen(req).read().decode("utf-8")
+    srchstr = "window._sharedData = "
+    s = s[s.find(srchstr)+len(srchstr):]
+    s = s[:s.find("};")+1]
+    s = json.loads(s)
+    #print("%s" % s['entry_data']['ProfilePage'][0]['user']['media']['nodes'][0]['code'])
+    return "https://www.instagram.com/p/" + s['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode']
+
 if __name__ == "__main__":
     print(search_untappd("heineken"))
