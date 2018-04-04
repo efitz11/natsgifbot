d139ae482ffe026e81b3e2fe2c1aa295342a4062
efitz11 <efitz11@gmail.com>
2018-04-03 21:05:41 -0400

    add checking of natsump tweets

--- redditbot.py
+++ redditbot.py
@@ -1,6 +1,7 @@
 import praw, prawcore.exceptions
 import pprint
 import gifs
+import json
 
 class RedditBot():
     def __init__(self):
@@ -61,9 +62,61 @@ class RedditBot():
                         f.write(submission.url)
             first = False
 
+class TwitterBot:
+    def __init__(self):
+        import tweepy
+        #read tokens
+        with open('keys.json','r') as f:
+            keys = f.read()
+
+        keys = json.loads(keys)
+        keys = keys['keys']
+        for key in keys:
+            if key['name'] == "twitter":
+                api_key = key['api_key']
+                secret = key['api_secret']
+                token = key['token']
+                token_secret = key['token_secret']
+            if key['name'] == "reddit":
+                self.r_cid = key['client_id']
+                self.r_user = key['user']
+                self.r_token = key['token']
+                self.r_pw = key['password']
+
+        auth = tweepy.OAuthHandler(api_key, secret)
+        auth.set_access_token(token, token_secret)
+        self.api = tweepy.API(auth)
+
+    def check_last_tweet(self):
+        #read last tweet
+        with open('lasttweet.txt','r') as f:
+            last_id = f.readline().strip()
+        # user = api.get_user('nationalsump')
+        # new_tweets = api.user_timeline(screen_name='nationalsump', count=1, tweet_mode="extended")
+        new_tweets = self.api.user_timeline(screen_name='nationalsump', count=1)
+        for tweet in new_tweets:
+            if str(tweet.id) != last_id:
+                print("new tweet %d" % tweet.id)
+                # post on reddit
+                reddit = praw.Reddit(client_id=self.r_cid,
+                      client_secret=self.r_token,
+                      user_agent='ubuntu:computer-dude (by /u/efitz11)',
+                      username=self.r_user,password=self.r_pw)
+
+                for submission in reddit.subreddit("nationals").new(limit=5):
+                    if submission.title.lower().startswith("game thread"):
+                        comment = tweet.text.replace("\n","\n\n")
+                        comment = comment + "\n\n" + tweet.entities['media'][0]['media_url_https']
+                        submission.reply(comment)
+
+                        with open('lasttweet.txt','w') as f:
+                            f.write(str(tweet.id))
+
 if __name__ == "__main__":
     r = RedditBot()
     # see if new gifs have been submitted to /r/nationalsgifs
     r.update_postlist()
     # respond to gif requests
     r.check_mentions()
+    t = TwitterBot()
+    t.check_last_tweet()
