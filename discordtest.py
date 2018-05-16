bd53be5ae6bb79d17dd161558723fdc395cb8acc
efitz11 <efitz11@gmail.com>
2018-05-16 14:43:56 -0400

    avoid embed caching

--- discordtest.py
+++ discordtest.py
@@ -240,7 +240,9 @@ async def forecast(*location:str):
 @bot.command()
 async def radar():
     """show pic of the current DC weather radar"""
-    await bot.say("https://api.weather.com/v2/maps/dynamic?geocode=38.85,-76.95&h=320&w=568&lod=8&product=twcRadarMosaic&map=light&format=jpg&language=en&apiKey=d522aa97197fd864d36b418f39ebb323&a=0")
+    randstr = "?%d" % random.randint(0,999)
+    await bot.say("https://api.weather.com/v2/maps/dynamic?geocode=38.85,-76.95&h=320&w=568&lod=8&product=twcRadarMosaic&map=light&format=jpg&language=en&apiKey=d522aa97197fd864d36b418f39ebb323&a=0"
+                  + randstr)
 
 def convert_number_to_emoji(num):
     snum = str(num)
