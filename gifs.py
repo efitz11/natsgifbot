e0fc85ad571be9b5a63fbd0eb3a967b85e3be4de
efitz11 <efitz11@gmail.com>
2018-07-02 14:11:00 -0400

    testing fuzzy gif search

--- gifs.py
+++ gifs.py
@@ -2,6 +2,7 @@ import re, random
 from urllib.request import urlopen, Request
 import urllib.parse
 import json
+from fuzzywuzzy import fuzz
 
 def gif(query):
     matches = []
@@ -27,7 +28,25 @@ def gif(query):
         return "no matches"
     num = random.randint(0,len(matches)-1)
     return matches[num].strip()
-    
+
+def fuzzygif(query):
+    highest,score = "",0
+    matches = []
+    f = open('postlist.csv','r')
+    for line in f:
+        search = ','.join(line.split(',')[:-1])
+        sc = fuzz.token_set_ratio(query,search)
+        if sc > score:
+            highest,score = line,sc
+            matches = [line]
+        elif sc == score:
+            matches.append(line)
+    f.close()
+    if len(matches) == 0:
+        return "no matches"
+    num = random.randint(0,len(matches)-1)
+    return matches[num].strip()
+
 def get_mlb_gif(query):
     url = "https://search-api.mlb.com/svc/search/v2/mlb_global_embed/query?expand=partner.media.photo&type=gif&q="+\
             urllib.parse.quote_plus("nationals " +query)+"&page=1&sort=new"
