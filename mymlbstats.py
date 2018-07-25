da5528087d626d8310b4f0486a6b93c50489019f
efitz11 <efitz11@gmail.com>
2018-07-25 01:12:00 -0400

    single digit extras in the linescore looks better

--- mymlbstats.py
+++ mymlbstats.py
@@ -399,8 +399,12 @@ def print_linescore(team, delta=None):
                     hr = str(inning['home']['runs'])
                 else:
                     hr = " "
-                lenstr = max(len(ar),len(hr),len(str(inning['num'])))
-                line0 = "%s %s" % (line0, str(inning['num']).rjust(lenstr))
+                inn = inning['num']
+                if inn > 9:
+                    inn -= 10
+                inn = str(inn)
+                lenstr = max(len(ar),len(hr),len(inn))
+                line0 = "%s %s" % (line0, inn.rjust(lenstr))
                 line1 = "%s %s" % (line1, ar.rjust(lenstr))
                 line2 = "%s %s" % (line2, hr.rjust(lenstr))
             away = game['linescore']['teams']['away']
