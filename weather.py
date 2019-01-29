f502afc4a7062682eea510aa01f56ff14671f6ab
efitz11 <efitz11@gmail.com>
2019-01-29 13:27:09 -0500

    print forecast location

--- weather.py
+++ weather.py
@@ -54,26 +54,6 @@ def get_current_weather(text):
     retval = "%sUpdated: %s" % (retval, updated)
     return "```python\n%s```" % retval
 
-    # req = Request("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+text+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
-    # req.headers["User-Agent"] = "windows 10 bot"
-    # # Load data
-    # data = json.loads(urlopen(req).read().decode("utf-8"))
-    # if data['query']['count']>0:
-    #     data = data['query']['results']['channel']
-    #     unit = data['units']['temperature']
-    #     condition = data['item']['condition']['text']
-    #     fcondition = data['item']['forecast'][0]['text']
-    #     if condition in emojimap:
-    #         condition = emojimap[condition]
-    #     if fcondition in emojimap:
-    #         fcondition = emojimap[fcondition]
-    #     hum=data['atmosphere']['humidity']
-    #     output = data['item']['title'] + '\n'
-    #     output = output + data['item']['condition']['temp'] + ' ' + unit + " - "
-    #     output = output + condition + "\tWind Chill: " + data['wind']['chill'] + ' ' + unit + '\tHumidity: ' + hum + "%\n"
-    #     output = output + data['item']['forecast'][0]['day'] + "'s forecast: " + data['item']['forecast'][0]['high'] + "/" + data['item']['forecast'][0]['low'] + ", " + fcondition
-    # return output
-    
 def get_forecast(text):
     '''get a 10 day weather forecast'''
     loc = get_lat_lon(text)
@@ -81,31 +61,25 @@ def get_forecast(text):
     print(url)
     req = requests.get(url)
     data = req.json()
+
+    stations_url = data['properties']['observationStations']
     forecast_url = data['properties']['forecast']
+
+    data = requests.get(stations_url).json()
+    station_name = data['features'][0]['properties']['name']
+
+    print(forecast_url)
     req = requests.get(forecast_url)
     data = req.json()
     periods = data['properties']['periods']
-    out = ""
+    out = "Forecast for %s:\n\n" % station_name
     if len(periods) > 0:
         for period in periods[:10]:
             out = out + "%s: %d%s\n%s\n\n" % (period['name'], period['temperature'], period['temperatureUnit'], period['detailedForecast'])
     return "```python\n%s```" % out
 
-    # if data['query']['count']>0:
-    #     data = data['query']['results']['channel']
-    #     city = data['location']['city']
-    #     region = data['location']['region']
-    #     output = "Forecast for %s, %s\n```python\n" % (city,region)
-    #     forecast = data['item']['forecast']
-    #     for day in forecast:
-    #         d = day['day']
-    #         h = day['high']
-    #         l = day['low']
-    #         c = day['text']
-    #         output = output + d.ljust(4) + h.rjust(3)+ "/" + l.ljust(3) + c + "\n"
-    #     return output + "```"
-
 def get_lat_lon(search):
+    #TODO: Cache locations using a dictionary
     query = search.replace(' ','+')
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&key="
     with open("keys.json",'r') as f:
@@ -117,6 +91,7 @@ def get_lat_lon(search):
         url = url + key
         req = Request(url, headers={'User-Agent' : "ubuntu"})
         s = json.loads(urlopen(req).read().decode("utf-8"))
+        # print(s)
         lat = s['results'][0]['geometry']['location']['lat']
         lon = s['results'][0]['geometry']['location']['lng']
         return(lat,lon)
