13c4ea7c569d08bf4903e54d6430b59f13511f40
efitz11 <efitz11@gmail.com>
2019-05-05 22:15:56 -0400

    added mostdongs

--- baseball.py
+++ baseball.py
@@ -351,7 +351,7 @@ class Baseball():
             team = ' '.join(team[1:]).lower()
             out = mymlbstats.print_broadcasts(team, delta=delta)
             await self.bot.say("```%s```" % out)
-        elif team[0] in ["longdongs", "shortdongs", "highdongs", "lowdongs", "fastdongs", "slowdongs"]:
+        elif team[0] in ["longdongs", "shortdongs", "highdongs", "lowdongs", "fastdongs", "slowdongs", "mostdongs"]:
             out = mymlbstats.print_dongs(team[0][:team[0].index('d')], delta=delta, reddit=reddit)
             await self.bot.say("```%s```" % out)
         elif team[0] in ["dongs", "newdongs"]:
