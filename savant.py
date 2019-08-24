a39444e1ce1da30864043a31e5fb9e9827d54036
efitz11 <efitz11@gmail.com>
2019-08-24 14:43:59 -0400

    added some game info to savant

--- savant.py
+++ savant.py
@@ -19,6 +19,29 @@ def get_game(gamepk):
     url = "https://baseballsavant.mlb.com/gf?game_pk=%s" % str(gamepk)
     return utils.get_json(url)
 
+def get_info_str(game_json):
+    home = dict()
+    away = dict()
+    home['team'] = game_json['home_team_data']['abbreviation']
+    away['team'] = game_json['away_team_data']['abbreviation']
+    home['r'] = game_json['scoreboard']['linescore']['teams']['home']['runs']
+    home['h'] = game_json['scoreboard']['linescore']['teams']['home']['hits']
+    home['e'] = game_json['scoreboard']['linescore']['teams']['home']['errors']
+    home['lob'] = game_json['scoreboard']['linescore']['teams']['home']['leftOnBase']
+    away['r'] = game_json['scoreboard']['linescore']['teams']['away']['runs']
+    away['h'] = game_json['scoreboard']['linescore']['teams']['away']['hits']
+    away['e'] = game_json['scoreboard']['linescore']['teams']['away']['errors']
+    away['lob'] = game_json['scoreboard']['linescore']['teams']['away']['leftOnBase']
+    if len(game_json['scoreboard']['stats']['wpa']['gameWpa']) > 0:
+        home['wpa'] = game_json['scoreboard']['stats']['wpa']['gameWpa'][-1]['homeTeamWinProbability']
+        away['wpa'] = game_json['scoreboard']['stats']['wpa']['gameWpa'][-1]['awayTeamWinProbability']
+
+    cols = ['team','r','h','e','lob','wpa']
+    left = ['team']
+    dicts = [away, home]
+
+    return utils.format_table(cols,dicts,left_list=left)
+
 def get_last_five(game_json):
     ev = game_json['exit_velocity']
     events = []
@@ -29,7 +52,8 @@ def get_last_five(game_json):
     cols = ['batter_name', 'result','hit_speed','hit_distance','hit_angle','xba']
     repl = {'batter_name':'batter','hit_speed':'EV','hit_distance':'dist','hit_angle':'LA'}
     left = ['batter_name', 'result']
-    output = "```python\n%s```" % utils.format_table(cols,events,repl_map=repl,left_list=left)
+    output = "```python\n%s```\n```python\n%s```" % (get_info_str(game_json),
+                                                     utils.format_table(cols,events,repl_map=repl,left_list=left))
     return output
 
 def get_player(game_json, playerid):
